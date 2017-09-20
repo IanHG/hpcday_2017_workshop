0b70437874564f323c1c33008c8ff30dbcc1f307
Ian Heide Godtliebsen <ian@chem.au.dk>
2017-09-20 16:08:46 +0200

    updated

--- 1_compile/main.c
+++ 1_compile/main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
+/* 
+ * Some preprocessor magic
+ */
+#define xstr(s) str(s)
+#define str(s) #s
+
+#if defined __INTEL_COMPILER
+   #define COMPILER intel-__INTEL_COMPILER
+#elif defined __GNUC__
+   #define COMPILER gcc-__GNUC__.__GNUC_MINOR__.__GNUC_PATCHLEVEL__
+#endif
+
+/* 
+ * Actual program
+ */
 int main(int argc, char* argv[])
 {
    printf("Hello World\n");
+   printf("Compiler version: " xstr(COMPILER) "\n");
+
    return 0;
 }
